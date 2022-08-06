b7349be1e1b3af30626041dd2a1f9fbba9415bba
Rion Halili <rionhalili@gmail.com>
2022-08-06 17:49:07 +0200

    Add Food, Medical and Other products

--- src/main/kotlin/products/MedicalProduct.kt
+++ src/main/kotlin/products/MedicalProduct.kt
@@ -1,4 +1,18 @@
 package products
 
-class MedicalProduct {
+import products.factory.MedicalProductFactory
+import products.factory.ProductFactory
+import tax.TaxType
+
+class MedicalProduct(quantity: Int, name: String, price: Double, imported: Boolean) :
+    Product(quantity, name, price, imported) {
+
+    override fun getFactory(): ProductFactory {
+        return MedicalProductFactory()
+    }
+
+    override fun getTaxValue(country: String): Double {
+        return if (country == "Local") TaxType.MEDICAL_TAX.taxValue else 0.0
+    }
 }
+
--- src/main/kotlin/products/OtherProduct.kt
+++ src/main/kotlin/products/OtherProduct.kt
@@ -1,4 +1,17 @@
 package products
 
-class OtherProduct {
+import products.factory.OtherProductsFactory
+import products.factory.ProductFactory
+import tax.TaxType
+
+class OtherProduct(quantity: Int, name: String, price: Double, imported: Boolean) :
+    Product(quantity, name, price, imported) {
+
+    override fun getFactory(): ProductFactory {
+        return OtherProductsFactory()
+    }
+
+    override fun getTaxValue(country: String): Double {
+        return if (country == "Local") TaxType.OTHER_TAX.taxValue else 0.0
+    }
 }
