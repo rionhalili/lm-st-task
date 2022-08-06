15447349d65283451663839ccc866b148fe7cf45
Rion Halili <rionhalili@gmail.com>
2022-08-06 18:57:20 +0200

    Implement toString method for better output

--- src/main/kotlin/receipt/Receipt.kt
+++ src/main/kotlin/receipt/Receipt.kt
@@ -6,4 +6,20 @@ data class Receipt(
     val products: List<Product>,
     val salesTaxes: Double,
     val total: Double
-)
+) {
+
+    override fun toString(): String {
+        var receipt = "\n\n"
+        receipt += "===============================\n"
+        receipt += "RECEIPT\n"
+        receipt += "===============================\n"
+        products.forEach { product ->
+            receipt += product.toString() + "\n"
+        }
+        receipt += "Sales taxes: $salesTaxes\n"
+        receipt += "Total: $total\n"
+        receipt += "===============================\n"
+
+        return receipt
+    }
+}
