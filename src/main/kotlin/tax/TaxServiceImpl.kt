bf0a8aff298d0b3559925776358db5936fabaa5c
Rion Halili <rionhalili@gmail.com>
2022-08-06 18:45:01 +0200

    Implement TaxService

--- src/main/kotlin/tax/TaxServiceImpl.kt
+++ src/main/kotlin/tax/TaxServiceImpl.kt
@@ -1,4 +1,14 @@
 package tax
 
-class TaxServiceImpl {
+class TaxServiceImpl : TaxService {
+    override fun calculateTax(price: Double, localTax: Double, imported: Boolean): Double {
+        var tax = price * localTax
+        if (imported) {
+            tax += 0.5 * price
+        }
+
+        tax = RoundingOperation.roundedValue(tax)
+        return tax
+
+    }
 }
