f7ea6eae17aaf3eb26217fdcfb90c83783b94ef1
Rion Halili <rionhalili@gmail.com>
2022-08-06 18:25:32 +0200

    Implement ProductFactory

--- src/main/kotlin/products/factory/ProductFactory.kt
+++ src/main/kotlin/products/factory/ProductFactory.kt
@@ -1,4 +1,7 @@
 package products.factory
 
-class ProductFactory {
+import products.Product
+
+abstract class ProductFactory {
+    abstract fun createProduct(quantity: Int, name: String, price: Double, imported: Boolean): Product
 }
